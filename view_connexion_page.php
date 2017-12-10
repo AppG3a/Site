e01ca3a1ec0d0ceb0e93d5310be2b7379350ab34
yixicheng <33733910+yixicheng@users.noreply.github.com>
2017-12-10 13:08:20 +0100

    Create view_connexion_page.php

---
+++ view_connexion_page.php
@@ -0,0 +1,16 @@
+<!doctype html> 
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>connexion</title>
+    </head>
+    <body>
+        <form name="login" action="index.php" method="post">
+                <p>pseodo<input type="text" name="pseudo"></p>
+               
+                <p>mot de passe<input type="password" name="password"></p>
+                <p><input type="submit" name="submit" value="connexion"></p>
+            </form>
+    </body>
+</html>
