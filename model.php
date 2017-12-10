335029f49538bbf2fc331a60df5036c5eb70b65c
yixicheng <33733910+yixicheng@users.noreply.github.com>
2017-12-10 13:09:23 +0100

    Update model.php

--- model.php
+++ model.php
@@ -1,14 +1,14 @@
 <?php
 
 function connexionbdd(){
-    $server="localhost:3306";//主机
-    $db_username="root";//你的数据库用户名
-    $db_password="root";//你的数据库密码
-    $con = mysqli_connect($server,$db_username,$db_password);//链接数据库
+    $server="localhost:3306";
+    $db_username="root";
+    $db_password="root";
+    $con = mysqli_connect($server,$db_username,$db_password);
     if(!$con){
-        die("can't connect".mysqli_error());//如果链接失败输出错误
+        die("can't connect".mysqli_error());
     }
-    $con->select_db('site_app');//选择数据库（我的是test）
+    $con->select_db('site_app');
     return $con;
 }
 
@@ -16,8 +16,8 @@ function connexionbdd(){
 
 function getprofile(){
     $bdd = connexionbdd();
-    $sql = "select * from utilisateurs where pseudo = '$name' and mot_de_passe='$password'";//检测数据库是否有对应的username和password的sql
-    $result = $con->query($sql);//执行sql
+    $sql = "select * from utilisateurs where pseudo = '$name' and mot_de_passe='$password'";
+    $result = $con->query($sql);
     $rows=mysqli_num_rows($result);
     return $rows;
 }
