103d4263305713ffdf7b8d2b6819e21fc7caf0b0
yixicheng <33733910+yixicheng@users.noreply.github.com>
2017-12-11 11:41:59 +0100

    Update model.php

--- model.php
+++ model.php
@@ -1,7 +1,7 @@
 <?php
 
 function connexionbdd(){
-    $server="localhost:3306";
+    $server="localhost";
     $db_username="root";
     $db_password="root";
     $con = mysqli_connect($server,$db_username,$db_password);
