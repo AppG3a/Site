abe8d639d4db759503f0360335ea2257f3151cbb
yixicheng <33733910+yixicheng@users.noreply.github.com>
2017-12-12 10:19:06 +0100

    Update controler.php

--- controler.php
+++ controler.php
@@ -5,12 +5,7 @@ function seeConnexionPage()
     require("view_connexion_page.php");
 }
 
-function getvalue(){
-    $name = $_POST['pseudo'];
-    $password = $_POST['password'];
-    return $name;
-    return $password;
-}
+
 
 
 ?>
